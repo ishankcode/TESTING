1a03b2d14af45eeed808cfa7ed5599f52fc8bfb3
ishank <ishank.gupta23@gmail.com>
2020-06-11 20:57:23 +0530

    first time

---
+++ palindrome.py
@@ -0,0 +1,16 @@
+s=input()
+
+
+def palin(s):
+    if len(s)<=1:
+        return True
+    else:
+        if s[0]==s[-1] and palin(s[1:-1]):
+            return True
+
+
+c=palin(s)
+if c==True:
+    print(s,"is palindrome")
+else:
+    print("not a palindrome")
